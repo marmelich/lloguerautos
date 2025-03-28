15f9605958bd355a0cf6b972ff46b94b5eff31ee
Mar <mar.melich@gmail.com>
2025-03-28 16:43:21 +0100

    Fita 5

--- lloguer/views.py
+++ lloguer/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from .models import Automobil
+
+def llistar_automobils(request):
+    automobils = Automobil.objects.all()
+    return render(request, 'automobils/llista_automobils.html', {'automobils': automobils})
--- lloguerautos/urls.py
+++ lloguerautos/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lloguer import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('automobils/', views.llistar_automobils, name='listar_automobiles'),
 ]
