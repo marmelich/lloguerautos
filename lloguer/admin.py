3329ed3d5ad4e0099101ca90c6965a311de31054
Mar <mar.melich@gmail.com>
2025-03-28 16:16:23 +0100

    Fita 3

--- lloguer/admin.py
+++ lloguer/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Automobil)
+
+@admin.register(Automobil)
+class AutomobilAdmin(admin.ModelAdmin):
+    list_display = ('marca', 'model', 'matricula')
+
+@admin.register(Reserva)
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ('automobil', 'user', 'data_inici', 'data_fi')
+    list_filter = ('data_inici', 'data_fi', 'automobil')
+
--- lloguer/models.py
+++ lloguer/models.py
@@ -9,3 +9,15 @@ class Automobil(models.Model):
 
     def __str__(self):
         return f"{self.matricula} - {self.marca} {self.model}"
+
+
+class Reserva(models.Model):
+    automobil = models.ForeignKey(Automobil, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    data_inici = models.DateField()
+    data_fi = models.DateField(blank=True, null=True)
+    class Meta:
+        unique_together = ('automobil', 'data_inici')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.automobil} del {self.data_inici} al {self.data_fi or '-'}"
